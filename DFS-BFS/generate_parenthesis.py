eea84efad72dd6fcd974868464d881b4d406afe7
Christian Morte <cmorte@uci.edu>
2017-11-13 21:14:46 -0800

    Create generate_parenthesis.py

---
+++ DFS-BFS/generate_parenthesis.py
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        if n <= 0: return []
+        
+        def dfs(p, n, L, R, s):
+            if L > n: return
+            if L < R: return
+            if L == R and L == n: return s.add(p)
+            dfs(p + '(', n, L + 1, R, s)
+            dfs(p + ')', n, L, R + 1, s)
+        
+        s = set()
+        dfs('(',n,1,0,s)
+        return list(s)
