8b80bd2ad7ebe1fd1de46de998c3e1a13d82ca8f
Christian Morte <cmorte@uci.edu>
2018-01-02 11:37:17 -0800

    Update minimum_window_substring.py

--- Strings/minimum_window_substring.py
+++ Strings/minimum_window_substring.py
@@ -1,26 +1,27 @@
 # https://leetcode.com/problems/minimum-window-substring/description/
 
+from collections import Counter
+
 class Solution(object):
     def minWindow(self, S, T):
         if not S or not T: return ''
-        if len(T) > len(S): return ''
-        V, C = {}, {c:0 for c in S}
-        for c in T:
-            if c not in V: V[c] = 0
-            V[c] += 1
-        i,j = 0,0
-        counter, res = 0, None
+        if len(T) == 1: return T if T in S else ''
+        i, j = 0, 0
+        Tc, curr, res = Counter(T), Counter(), S+'_'
         while j < len(S):
-            if S[j] in V:
-                C[S[j]] += 1
-                counter += 1 if C[S[j]] <= V[S[j]] else 0
-            if counter == len(T):
-                while counter == len(T):
-                    if S[i] in V:
-                        C[S[i]] -= 1
-                        counter -= 1 if C[S[i]] < V[S[i]] else 0
-                    i += 1
-                if not res: res = S[i-1:j+1]
-                else: res = min(res, S[i-1:j+1], key=len)
-            j += 1
-        return res if res else ''
+            while j < len(S) and not self.isValid(curr, Tc):
+                if S[j] in Tc: curr[S[j]] += 1
+                j += 1
+            while i < j and self.isValid(curr, Tc):
+                res = min(res, S[i:j], key=len)
+                if S[i] in Tc: curr[S[i]] -= 1
+                i += 1
+            while i < j and S[i] not in Tc:
+                i += 1
+        return '' if len(res) > len(S) else res
+            
+    def isValid(self, curr, T):
+        if len(curr) < len(T): return False
+        for c in curr:
+            if curr[c] < T[c]: return False
+        return True
